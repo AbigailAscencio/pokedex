6c50f915203e973f158528b7eec90d588df0cd56
Abigail Ascencio <a.ascencio@akron.com.mx>
2022-10-05 00:00:39 -0500

    Se integra informacion del API

--- main.js
+++ main.js
@@ -1,122 +1,130 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
-const spinner = document.querySelector("#spinner");
-const previous = document.querySelector("#previous");
-const next = document.querySelector("#next");
+const spinner = document.querySelector("#spinner"); // tarerlo por ID
+const previous = document.querySelector("#previous");// traer los botones por ID
+const next = document.querySelector("#next");//  traer los botones por ID
 
-let limit = 8;
-let offset = 1;
+let limit = 11;
+let idPokemon = 1;
 
 previous.addEventListener("click", () => {
-  if (offset != 1) {
-    offset -= 9;
-    removeChildNodes(pokemonContainer);
-    fetchPokemons(offset, limit);
+  if (idPokemon != 1) { // que no sea igual a 1 porque si estamos en la pagina 1 ya no podemos regresar mas
+    idPokemon -= 12;
+    removeChildNodes(pokemonContainer); // Llamamos la funcion para remover los pokemones de la pagina
+    fetchPokemons(idPokemon, limit);
   }
 });
 
 next.addEventListener("click", () => {
-  offset += 9;
+  idPokemon += 12;
   removeChildNodes(pokemonContainer);
-  fetchPokemons(offset, limit);
+  fetchPokemons(idPokemon, limit);
 });
 
-function fetchPokemon(id) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((res) => res.json())
-    .then((data) => {
-      createPokemon(data);
-      spinner.style.display = "none";
+function fetchPokemon(id) { // traer el pokemon por medio de la API
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)  // URL API
+    .then((res) => res.json())  //Respuesta con el .json para manejar la promesa con fetch
+    .then((data) => { // informacion que se trae
+      createPokemon(data); // llamamos la funcion de la creacion de tarjetas
+      console.log(data)
+      spinner.style.display = "none";//llamamos el spinner y lo ocultamos
     });
 }
 
-function fetchPokemons(offset, limit) {
-  spinner.style.display = "block";
-  for (let i = offset; i <= offset + limit; i++) {
-    fetchPokemon(i);
+function fetchPokemons(idPokemon, limit) { // funcion que hace el recorrido para traer los pokemones
+  spinner.style.display = "block"; //mostramos el Spinner
+  for (let i = idPokemon; i <= idPokemon + limit; i++) {
+    fetchPokemon(i);// LLama la funcion que trae los datos y trae el numero de iteracion
   }
 }
 
-function createPokemon(pokemon) {
-  const flipCard = document.createElement("div");
-  flipCard.classList.add("flip-card");
+function createPokemon(pokemon) { // craciones de las tarjetas 
+  const flipCard = document.createElement("div");// crear la flip card
+  flipCard.classList.add("flip-card");// agregamos la clase
 
-  const cardContainer = document.createElement("div");
+  const cardContainer = document.createElement("div");//contenedor de la flip card
   cardContainer.classList.add("card-container");
 
-  flipCard.appendChild(cardContainer);
+  flipCard.appendChild(cardContainer);// Añadiendo el flipcard
 
-  const card = document.createElement("div");
+  const card = document.createElement("div"); // crear la tarejtas
   card.classList.add("pokemon-block");
 
-  const spriteContainer = document.createElement("div");
+  const spriteContainer = document.createElement("div"); // contenedor de la imagen
   spriteContainer.classList.add("img-container");
 
-  const sprite = document.createElement("img");
-  sprite.src = pokemon.sprites.front_default;
+  const sprite = document.createElement("img"); // Traer la imagen 
+  sprite.src = pokemon.sprites.front_default; // trae la url de la imagen de la API con la ruta sprite y front_default 
 
-  spriteContainer.appendChild(sprite);
+  spriteContainer.appendChild(sprite); // añadir el hijo
 
-  const number = document.createElement("p");
-  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+  const number = document.createElement("p");// agregar parrafo para el id del pokemon 
+  number.textContent = `ID: ${pokemon.id}`;  //URL de donde traera el ID de la API
 
-  const name = document.createElement("p");
-  name.classList.add("name");
-  name.textContent = pokemon.name;
+  const name = document.createElement("p"); // agregar parrafo para el nombre del pokemon
+  name.classList.add("name"); 
+  name.textContent = pokemon.name; //URL de donde traera el ID de la API
 
-  card.appendChild(spriteContainer);
+  card.appendChild(spriteContainer); // agregarlos a la tarjeta
   card.appendChild(number);
   card.appendChild(name);
 
-  const cardBack = document.createElement("div");
+  // creacion de la parte de atras de la carta(flip-card)
+  const cardBack = document.createElement("div");//div de la carta de atras
   cardBack.classList.add("pokemon-block-back");
+  cardBack.textContent = "Información"; // texto
+ 
 
-  cardBack.appendChild(progressBars(pokemon.stats));
+  cardBack.appendChild(habilidades(pokemon.abilities));
+  cardBack.appendChild(pesos(pokemon.weight));
 
-  cardContainer.appendChild(card);
+  cardContainer.appendChild(card); // agregarlos a la tarjeta
   cardContainer.appendChild(cardBack);
   pokemonContainer.appendChild(flipCard);
 }
 
-function progressBars(stats) {
-  const statsContainer = document.createElement("div");
-  statsContainer.classList.add("stats-container");
+//barras de
 
-  for (let i = 0; i < 3; i++) {
-    const stat = stats[i];
+function habilidades(abilities) {
+  const abilitiesContainer = document.createElement("div");
+  abilitiesContainer.classList.add("informacion-container");
+  for (let i = 0; i <= 1; i++) {
+    const ability = abilities[i];
 
-    const statPercent = stat.base_stat / 2 + "%";
-    const statContainer = document.createElement("stat-container");
-    statContainer.classList.add("stat-container");
+    const abilityContainer = document.createElement("informacion-container");
+    abilityContainer.classList.add("informacion-container");
 
-    const statName = document.createElement("p");
-    statName.textContent = stat.stat.name;
+    const abilityName = document.createElement("p");
+    abilityName.textContent = `Habilidad: ${ability.ability.name}` ;
 
-    const progress = document.createElement("div");
-    progress.classList.add("progress");
-
-    const progressBar = document.createElement("div");
-    progressBar.classList.add("progress-bar");
-    progressBar.setAttribute("aria-valuenow", stat.base_stat);
-    progressBar.setAttribute("aria-valuemin", 0);
-    progressBar.setAttribute("aria-valuemax", 200);
-    progressBar.style.width = statPercent;
-
-    progressBar.textContent = stat.base_stat;
-
-    progress.appendChild(progressBar);
-    statContainer.appendChild(statName);
-    statContainer.appendChild(progress);
-
-    statsContainer.appendChild(statContainer);
+    abilityContainer.appendChild(abilityName);
+    abilitiesContainer.appendChild(abilityContainer);
   }
 
-  return statsContainer;
+  return abilitiesContainer;
+}
+function pesos(weight) {
+  const weightContainer = document.createElement("div");
+  weightContainer.classList.add("informacion-container");
+  for (let i = 0; i < 1; i++) {
+    const peso = weight[i];
+
+    const pesoContainer = document.createElement("informacion-container");
+    pesoContainer.classList.add("informacion-container");
+
+    const pesoName = document.createElement("p");
+    pesoName.textContent = `Peso: ${weight} kg` ;
+
+    pesoContainer.appendChild(pesoName);
+    weightContainer.appendChild(pesoContainer);
+  }
+
+  return weightContainer;
 }
 
-function removeChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+function removeChildNodes(parent) { // remover los elementos de la paginacion 
+  while (parent.firstChild) { //mientras se tenga tarejtas
+    parent.removeChild(parent.firstChild); // remover los elementos
   }
 }
 
-fetchPokemons(offset, limit);
+fetchPokemons(idPokemon, limit);
